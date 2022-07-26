a4a27624fccf30a2197956343814447ed593dfc6
ThanuraSilva <thanurasilva1989@gmail.com>
2022-07-26 21:34:45 +0530

    Project Init with expose Controller

---
+++ src/test/java/com/thanu/home/AppInitializerTests.java
@@ -0,0 +1,13 @@
+package com.thanu.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppInitializerTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
