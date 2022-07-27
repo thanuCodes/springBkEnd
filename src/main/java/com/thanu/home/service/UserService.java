fd198ccf3c59c5ee59d9131fe85189be965155ec
ThanuraSilva <thanurasilva1989@gmail.com>
2022-07-27 06:54:42 +0530

    User Service Expose

--- src/main/java/com/thanu/home/service/UserService.java
+++ src/main/java/com/thanu/home/service/UserService.java
@@ -1,5 +1,13 @@
 package com.thanu.home.service;
 
+import com.thanu.home.dto.UserDTO;
+
 public interface UserService {
+
+    UserDTO registerUser (UserDTO userDTO);
+    void updateUser(UserDTO userDTO);
+    UserDTO getUserInfo(String userId);
+    void deleteUser (String userId);
+
 
 }
